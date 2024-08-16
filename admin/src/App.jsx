deb65d236df3fa8b303e58c929756df4e969e8c2
Ajay07102000 <18ajaysaini@gmail.com>
2024-08-16 18:20:43 +0530

    Changing URL of admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
   
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://food-del-app-backend-e9d2.onrender.com";
 
   return (
     <div>
